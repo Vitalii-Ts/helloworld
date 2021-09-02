9f2edb53ec9bc450a8e595b6242312c614ec827d
Vitalii-Ts <vitalii.tskhovrebashvili@gmail.com>
2021-09-02 10:18:35 +0300

    task №1

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Привет Виталик");
+        System.out.println(FirstClass.sum(2, 2));
+        System.out.println(FirstClass.sum(2, 2));
+    }
+}
